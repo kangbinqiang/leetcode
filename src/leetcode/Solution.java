81d43ecd13297ff157992057a16fffea6ed6f6c8
voyage <18822253832@163.com>
2021-08-15 12:43:31 +0800

    第二题：两数相加

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -694,6 +694,84 @@ public class Solution {
     }
 
 
+    public static HashMap<Integer, Integer> treeMap = new HashMap<>();
+
+    /**
+     * 给定二叉树的前序遍历和中序遍历结果，生成二叉树
+     * Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+     * Output: [3,9,20,null,null,15,7]
+     * 1、首先二叉树的前序遍历结果中，第一个节点就是根节点
+     * 2、在二叉树中序遍历结果中找出根节点所在的下标，则左边的为左子树，右边的为右子树
+     * 3、每一个左子树序列又能在前序中找出根节点，依次循环1、2、3即可完成二叉树的创建
+     *
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int len = preorder.length;
+
+        //将中序结果保存在一个字典中
+        for (int i = 0; i < inorder.length; i++) {
+            treeMap.put(inorder[i], i);
+        }
+        return build(preorder, 0, len - 1, inorder, 0, len - 1);
+
+    }
+
+    public TreeNode build(int[] preorder, int pre_left, int pre_right, int[] inorder, int in_left, int in_right) {
+        if (pre_left > pre_right) {
+            return null;
+        }
+        //前序遍历结果根节点所在的位置为第一个，试想：假如换成后序遍历，则最后一个为根节点
+        int pre_root = 0;
+        //中序遍历结果中根节点所在的位置
+        int in_root = treeMap.get(preorder[pre_root]);
+        //左子树的长度
+        int left_size = in_root - in_left;
+        TreeNode root = new TreeNode(preorder[pre_root]);
+        //找出前序和中序遍历结果中的左子树
+        root.left = build(preorder, pre_left + 1, pre_left + left_size, inorder, in_right, left_size - 1);
+        //找出前序和中序遍历结果中的右子树
+        root.right = build(preorder, pre_left + left_size + 1, pre_right, inorder, left_size + 1, in_right);
+        return root;
+    }
+
+
+    /**
+     * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int flag = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + flag;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            flag = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (flag > 0) {
+            tail.next = new ListNode(flag);
+        }
+        return head;
+    }
+
+
     public static void main(String[] args) {
 //        int[][] times = new int[][]{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}};
 //        int n = 4;
